03a1199bcdaa52ef90ef05c119b0c154287ef7d3
burnout156 <brunoferreira378@gmail.com>
2021-04-02 19:37:01 -0300

    navmesh funcional

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,20 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public EnemyModel model;
+    public List<GameObject> checkPoints; //aqui é para achar os pontos que o inimigo poderá ir
+    public GameObject[] arrayCheckPoints;
+    public NavMeshAgent navMesh;
+    public Player player;
+    public List<int> indexCheckPoints;
+    public int indexNow;
 
-    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Velocity: " + model.velocity);
+        player = FindObjectOfType<Player>();
+        navMesh = GetComponent<NavMeshAgent>();
+        checkPoints.AddRange(GameObject.FindGameObjectsWithTag("checkpoint"));
+        SetPathsRandom();
+        navMesh.SetDestination(checkPoints[indexCheckPoints[indexNow]].transform.position);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetPathsRandom()
     {
-        
+        for(int contador = 0; contador < checkPoints.Count - 1;)
+        {
+            int randomNumber = Random.Range(0, checkPoints.Count - 1);
+
+            if(!indexCheckPoints.Contains(randomNumber))
+            {
+                indexCheckPoints.Add(randomNumber);
+                contador++;
+            }
+        }
+
+        arrayCheckPoints = new GameObject[checkPoints.Count];
+
+        for (int contador2 = 0; contador2 < checkPoints.Count - 1; contador2++)
+        {
+            arrayCheckPoints[contador2] = checkPoints[contador2];
+        }
     }
+
+    private void FixedUpdate()
+    {
+        var distance = Vector3.Distance(checkPoints[indexCheckPoints[indexNow]].transform.position, transform.position);
+
+        if(distance < 0.5f)
+        {
+            if (indexNow + 1 >= checkPoints.Count - 1)
+            {
+                indexNow = 0;
+                navMesh.destination = (checkPoints[indexCheckPoints[indexNow]].transform.position);
+            }
+
+            else
+            {
+                indexNow++;
+                navMesh.destination = (checkPoints[indexCheckPoints[indexNow]].transform.position);
+            }
+        }
+    }
+
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float velocity = 5; // Velocidade que irá ser para mover o jogador
+
+    private Vector3 direction; //para saber qual direção o jogador quer se mvoer
+    private Rigidbody rigidBody;
+    [SerializeField] private float horizontal;
+    [SerializeField] private float vertical;
+
+    private void Start()
+    {
+        if(velocity == 0)
+        {
+            velocity = 5;
+        }
+
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");      
+        direction = new Vector3(horizontal, 0, vertical);
+
+        Move(direction);
+    }
+
+    public void Move(Vector3 moveDirection)
+    {
+        rigidBody.velocity = (moveDirection * velocity);
+    }
+  
+}
