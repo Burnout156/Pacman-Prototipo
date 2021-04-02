cd269396e962172150664a467d291d729b28f24e
burnout156 <brunoferreira378@gmail.com>
2021-04-02 14:08:22 -0300

    1 commit com scriptable feito e ainda organizando a fase

---
+++ Assets/Scripts/ScriptableObjects/EnemyModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/EnemyModel", order = 1)]
+public class EnemyModel : ScriptableObject
+{
+    public float velocity;
+}
