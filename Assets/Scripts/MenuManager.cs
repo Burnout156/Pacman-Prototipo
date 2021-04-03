2f5a4b2513d3eaa2155945bd60f3da2e0a5f3ef3
burnout156 <brunoferreira378@gmail.com>
2021-04-03 15:10:46 -0300

    adicionado o level2 e o menu também

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject panelMainMenu;
+    public GameObject panelSelectLevel;
+
+    void Start()
+    {
+        if(panelMainMenu == null)
+        {
+            panelMainMenu = GameObject.Find("PanelMenu");
+        }
+
+        if (panelSelectLevel == null)
+        {
+            panelSelectLevel = GameObject.Find("PanelSelectLevel");
+        }
+
+        panelSelectLevel.SetActive(false);
+    }
+
+    public void SelectLevel()
+    {
+        panelMainMenu.SetActive(false);
+        panelSelectLevel.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Menu()
+    {
+        panelSelectLevel.SetActive(false);
+        panelMainMenu.SetActive(true);
+    }
+}
+
